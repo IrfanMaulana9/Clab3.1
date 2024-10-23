5c72d0fdcb98c89679ae92e7ca07ba2744965819
irfanmaulana-480 <145763624+IrfanMaulana9@users.noreply.github.com>
2024-10-23 20:26:33 +0700

    Add files via upload

---
+++ DependencyPractice/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        // Output asli tanpa pembalikan teks
+        String text = "Hello, Maven!";
+        System.out.println(text);
+    }
+}
